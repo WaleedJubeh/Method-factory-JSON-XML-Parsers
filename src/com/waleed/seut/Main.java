02c270301850100ed19175bb5cea480d7e35131b
Waleed Jubeh <wsameer70@gmail.com>
2019-08-09 17:37:37 +0300

    delete unnessary files

--- src/com/waleed/seut/Main.java
+++ src/com/waleed/seut/Main.java
@@ -1,15 +1,8 @@
 package com.waleed.seut;
-
-import org.apache.xmlbeans.XmlOptions;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
-import javax.xml.crypto.dsig.XMLValidateContext;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.validation.Validator;
 import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) throws IOException, ParseException, XMLStreamException {
